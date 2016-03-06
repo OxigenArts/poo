762ee9859949d43106ab6e9a01021f52060d23b3
Pablo Androetto <pabloandroetto@outlook.com>
2016-03-06 01:15:53 -0300

    four

--- vistas/temas/readonly/four.php
+++ vistas/temas/readonly/four.php
@@ -1,23 +1,32 @@
+<?php
+require_once('classes/config.php');
+require_once('classes/temas.php');
+require_once('classes/posts.php');
+require_once('classes/imagenes.php');
+$conf = new Config();
+$tema = new Tema();
+$tema->setId($conf->getCfg("tema"));
+?>
 <section id="four">
 	<div class="container">
-		<h3>Contact Me</h3>
-		<p>Integer eu ante ornare amet comvestibulum blandit integer in curfaucibus integer non. Adipiscing cuelementum integer. Integer eu ante oamet commetus.</p>
+		<h3><?php echo $tema->getCfg("contact_title"); ?></h3>
+		<p><?php echo $tema->getCfg("contact_content"); ?></p>
 		<form method="post" action="#">
 			<div class="row uniform collapse-at-2">
-				<div class="6u"><input type="name="name" id="name" placehoName" /></div>
-				<div class="6u"><input type="ename="email" id="email" placehoEmail" /></div>
+				<div class="6u"><input type="text" name="name" id="name" placeholder="Nombre" /></div>
+				<div class="6u"><input type="email" name="email" id="email" placeholder="Email" /></div>
 			</div>
 			<div class="row uniform">
-				<div class="12u"><input type="name="subject" id="subplaceholder="Subject" /></div>
+				<div class="12u"><input type="text" name="subject" id="subject" placeholder="Asunto" /></div>
 			</div>
 			<div class="row uniform">
-				<div class="12u"><textarea message" id="message" placehoMessage" rows="6"></textarea></div>
+				<div class="12u"><textarea name="message" id="message" placeholder="Mensaje" rows="6"></textarea></div>
 			</div>
 			<div class="row uniform">
 				<div class="12u">
 					<ul class="actions">
-						<li><input type="suclass="special" value=Message" /></li>
-						<li><input type="reset"="Reset Form" /></li>
+						<li><input type="submit" class="special" value="Enviar Mensaje" /></li>
+						<li><input type="reset" value="Limpiar Formulario" /></li>
 					</ul>
 				</div>
 			</div>
