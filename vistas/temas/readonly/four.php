d63408d7e15f425923762f99a1ed428e6346e3c6
Pablo Androetto <pabloandroetto@outlook.com>
2016-03-06 00:58:29 -0300

    Aasd

---
+++ vistas/temas/readonly/four.php
@@ -0,0 +1,26 @@
+<section id="four">
+	<div class="container">
+		<h3>Contact Me</h3>
+		<p>Integer eu ante ornare amet comvestibulum blandit integer in curfaucibus integer non. Adipiscing cuelementum integer. Integer eu ante oamet commetus.</p>
+		<form method="post" action="#">
+			<div class="row uniform collapse-at-2">
+				<div class="6u"><input type="name="name" id="name" placehoName" /></div>
+				<div class="6u"><input type="ename="email" id="email" placehoEmail" /></div>
+			</div>
+			<div class="row uniform">
+				<div class="12u"><input type="name="subject" id="subplaceholder="Subject" /></div>
+			</div>
+			<div class="row uniform">
+				<div class="12u"><textarea message" id="message" placehoMessage" rows="6"></textarea></div>
+			</div>
+			<div class="row uniform">
+				<div class="12u">
+					<ul class="actions">
+						<li><input type="suclass="special" value=Message" /></li>
+						<li><input type="reset"="Reset Form" /></li>
+					</ul>
+				</div>
+			</div>
+		</form>
+	</div>
+</section>
